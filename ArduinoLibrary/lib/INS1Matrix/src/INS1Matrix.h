2708e6ef376f70ab86e9dfe5101968668f65a43a
Nathan <nsafran1217@gmail.com>
2023-09-09 01:16:40 -0400

    Change to have animation info in data

--- ArduinoLibrary/lib/INS1Matrix/src/INS1Matrix.h
+++ ArduinoLibrary/lib/INS1Matrix/src/INS1Matrix.h
@@ -10,9 +10,9 @@ Nathan Safran - 9/10/2023
 class INS1Matrix
 {
   public:
-    INS1Matrix(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t blankPin, boolean inverted);
-    void writeStaticImgToDisplay(uint32_t imgData[], uint8_t displays); // write 2 uint32_t to the display * the num of displays. imgData should be an array of uint32_t 2*displays big
-    void setAnimationToDisplay(const uint32_t* animationImgData, uint8_t displays, uint8_t frames, uint8_t delay); // write 2 uint32_t to display * the num of displays, how many frames are in the animation data, the delay between frames in ms. imgData should be a const array of frames (array of uint32_t 2*displays big)
+    INS1Matrix(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t blankPin, boolean inverted, uint8_t displays);
+    void writeStaticImgToDisplay(uint32_t imgData[]); // write 2 uint32_t to the display * the num of displays. imgData should be an array of uint32_t 2*displays big
+    void setAnimationToDisplay(const uint32_t* animationImgData); // write 2 uint32_t to display * the num of displays, how many frames are in the animation data, the delay between frames in ms. imgData should be a const array of frames (array of uint32_t 2*displays big)
     void animateDisplay();
 
     
@@ -22,15 +22,15 @@ class INS1Matrix
     uint8_t _clockPin;
     uint8_t _latchPin;
     uint8_t _blankPin;
+    boolean _inverted;  //since the shift register is 12v logic level, its mostly likely going to be driven by an inverted signal. Account for this in the code.
+    uint8_t _displays;
     uint8_t _highValue;
     uint8_t _lowValue;
     const uint32_t* _animationImgData;
-    uint8_t _displays;
-    uint8_t _frames;
-    uint8_t _frameDelay;
-    boolean _inverted;  //since the shift register is 12v logic level, its mostly likely going to be driven by an inverted signal. Account for this in the code.
-    uint32_t _timeSinceLastFrame;  //time since last frame was displayed
-    uint8_t _lastFrame; //last frame sent to display
+    int _frames;
+    int _frameDelay;
+    uint32_t _timeOfLastFrame;  //time since last frame was displayed
+    int _lastFrame; //last frame sent to display
 
 };
 
